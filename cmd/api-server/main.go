86481c9120bd4e4c1f75b84de226298a6009e12d
Huy <dinhhuypfiev@gmail.com>
2024-10-19 19:01:11 +0200

    chore: set up generic server and log configuration

--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -3,34 +3,63 @@ package main
 import (
 	"context"
 	"flag"
-	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
 	"time"
 
+	"github.com/hamba/logger/v2"
+	"github.com/hamba/pkg/v2/http/server"
+	"github.com/hamba/statter/v2"
 	"github.com/huy125/financial-data-web/api"
 	"github.com/huy125/financial-data-web/api/store"
 )
 
 func main() {
-	var apiKey string
+	var (
+		apiKey string
+		host string
+		port int
+		env string
+	)
+
 	flag.StringVar(&apiKey, "apiKey", "", "Alpha Vantage API Key, required for stocks endpoints")
+	flag.StringVar(&host, "host", "localhost", "Server host, the default value is localhost")
+	flag.IntVar(&port, "port", 8080, "Listen port for server, the default value is 8080")
+	flag.StringVar(&env, "env", "dev", "Environment where the server runs on, the default value is development")
 	flag.Parse()
 
+	minLvl := logger.Debug
+	if env == "production" {
+		minLvl = logger.Info
+	}
+
+	log := logger.New(os.Stdout, logger.JSONFormat(), minLvl)
+	cancel := log.WithTimestamp()
+	defer cancel()
+
+	stats := statter.New(statter.DiscardReporter, 10*time.Second)
 	if apiKey == "" {
-		log.Fatal("apiKey is required")
+		log.Error("apiKey is required")
 	}
 
-	store := store.NewInMemory()
-	srv := api.New(apiKey, store)
-
-	log.Println("Starting server on port :8080")
-	err := http.ListenAndServe(":8080", srv)
-	if err != nil {
-		log.Fatal("Failed to start server:", err)
+	addr := host + ":" + strconv.Itoa(port)
+	h := api.New(apiKey, store)
+	server := server.GenericServer[context.Context]{
+		Addr: addr,
+		Handler: h,
+		Log: log,
+		Stats: stats,
 	}
 
-	log.Println("Server terminated")
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+	if err := server.Run(ctx); err != nil && err != http.ErrServerClosed {
+		log.Error("Could not start server")
+		cancel()
+	}
+
+	log.Info("Server terminated")
 }
