2c1206c77c0940ba2d5e9931bf8c6b663fd3e307
Huy <dinhhuypfiev@gmail.com>
2024-11-10 21:47:44 +0100

    feat: add mutex to in_memory.go

--- api/store/in_memory.go
+++ api/store/in_memory.go
@@ -2,21 +2,24 @@ package store
 
 import (
 	"context"
+	"sync"
 
 	model "github.com/huy125/financial-data-web/api/models"
 )
 
 type InMemory struct {
+	mu sync.Mutex
 	users []model.User
 }
 
-func NewInMemory()(*InMemory, error) {
-	return &InMemory{
-		users: []model.User{},
-	}, nil
+func NewInMemory() (*InMemory, error) {
+	return &InMemory{}, nil
 }
 
 func (s *InMemory) Create(ctx context.Context, user model.User) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	
 	user.ID = len(s.users) + 1
 	s.users = append(s.users, user)
 
