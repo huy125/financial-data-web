2690e782d632876fa27118068a4766e031a79512
Huy <dinhhuypfiev@gmail.com>
2024-11-17 18:13:32 +0100

    feat: add unit tests for updating users

--- api/users_test.go
+++ api/users_test.go
@@ -8,6 +8,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/huy125/financial-data-web/api"
 	model "github.com/huy125/financial-data-web/api/models"
 	"github.com/stretchr/testify/assert"
@@ -20,23 +21,27 @@ type storeMock struct {
 }
 
 func (m *storeMock) Create(ctx context.Context, user model.User) error {
-	m.Called(user)
-	return nil
+	args := m.Called(user)
+	return args.Error(0)
 }
 
 func (m *storeMock) List(ctx context.Context, limit, offset int) ([]model.User, error) {
-	return []model.User{
-		{
-			ID:       1,
-			Username: "test@xyz.com",
-			Hash:     "hashedpassword1",
-		},
-		{
-			ID:       2,
-			Username: "example@abc.com",
-			Hash:     "hashedpassword2",
-		},
-	}, nil
+	args := m.Called(ctx, limit, offset)
+	return args.Get(0).([]model.User), args.Error(1)
+}
+
+func (m *storeMock) Find(ctx context.Context, id uuid.UUID) (*model.User, error) {
+	args := m.Called(ctx, id)
+	if user, ok := args.Get(0).(*model.User); ok {
+		return user, args.Error(1)
+	}
+
+	return nil, args.Error(1)
+}
+
+func (m *storeMock) Update(ctx context.Context, id uuid.UUID, userUpdate model.UserUpdate) error {
+	args := m.Called(ctx, id, userUpdate)
+	return args.Error(0)
 }
 
 func TestServer_CreateUserHandler(t *testing.T) {
@@ -46,7 +51,7 @@ func TestServer_CreateUserHandler(t *testing.T) {
 		sendBody           string
 		wantUser           model.User
 		expectedStatusCode int
-		expectedResponse      any
+		expectedResponse   any
 	}{
 		{
 			name: "creates user successfully",
@@ -55,7 +60,7 @@ func TestServer_CreateUserHandler(t *testing.T) {
 
 			wantUser:           model.User{Username: "test@example.com", Hash: "hashedpassword"},
 			expectedStatusCode: http.StatusCreated,
-			expectedResponse: "User created successfully",
+			expectedResponse:   "User created successfully",
 		},
 	}
 
@@ -89,6 +94,99 @@ func TestServer_CreateUserHandler(t *testing.T) {
 			err := json.Unmarshal(rr.Body.Bytes(), &res)
 			require.NoError(t, err)
 			assert.Equal(t, test.expectedResponse, res)
+
+			store.AssertExpectations(t)
+		})
+	}
+}
+
+func TestServer_UpdateUserHandler(t *testing.T) {
+	id := uuid.New()
+	tests := []struct {
+		name string
+
+		uri      string
+		sendBody string
+
+		existUser      *model.User
+		updateUser     model.UserUpdate
+		shouldMockFind bool
+
+		expectedStatusCode int
+		expectedResponse   any
+	}{
+		{
+			name:     "updates user successfully",
+			uri:      "/users/" + id.String(),
+			sendBody: `{"username":"test1@example.com"}`,
+
+			existUser:      &model.User{ID: id, Username: "test@example.com", Hash: "hashpassword"},
+			updateUser:     model.UserUpdate{Username: "test1@example.com"},
+			shouldMockFind: true,
+
+			expectedStatusCode: http.StatusOK,
+			expectedResponse:   "User updated successfully",
+		},
+		{
+			name:     "handles user not found",
+			uri:      "/users/" + id.String(),
+			sendBody: `{"username":"test1@example.com"}`,
+
+			existUser:      nil,
+			updateUser:     model.UserUpdate{},
+			shouldMockFind: true,
+
+			expectedStatusCode: http.StatusNotFound,
+		},
+		{
+			name:     "handles invalid id",
+			uri:      "/users/invalid-id",
+			sendBody: `{"username":"test1@example.com"}`,
+
+			existUser:      nil,
+			updateUser:     model.UserUpdate{},
+			shouldMockFind: false,
+
+			expectedStatusCode: http.StatusBadRequest,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			store := &storeMock{}
+			if test.shouldMockFind {
+				store.On("Find", mock.Anything, id).Return(test.existUser, nil)
+			}
+
+			if test.updateUser != (model.UserUpdate{}) {
+				store.On("Update", mock.Anything, id, test.updateUser).Return(nil)
+			}
+
+			apiKey := "testApiKey"
+			srv := api.New(apiKey, store)
+
+			httpSrv := httptest.NewServer(srv)
+			t.Cleanup(func() { httpSrv.Close() })
+
+			reqURL := httpSrv.URL + test.uri
+
+			var req *http.Request
+			req, _ = http.NewRequest(http.MethodPatch, reqURL, bytes.NewBufferString(test.sendBody))
+
+			rr := httptest.NewRecorder()
+
+			srv.ServeHTTP(rr, req)
+
+			assert.Equal(t, test.expectedStatusCode, rr.Code)
+
+			if rr.Code == http.StatusOK {
+				var res string
+				err := json.Unmarshal(rr.Body.Bytes(), &res)
+				require.NoError(t, err)
+				assert.Equal(t, test.expectedResponse, res)
+			}
 
 			store.AssertExpectations(t)
 		})
