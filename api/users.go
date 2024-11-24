485eabefd9f5fd5ef15cf9215c30ef46c28e64ed
Huy <dinhhuypfiev@gmail.com>
2024-11-24 23:03:57 +0100

    feat: optimize some nitpicks

--- api/users.go
+++ api/users.go
@@ -87,7 +87,6 @@ func (h *UserHandler) UpdateUserHandler(w http.ResponseWriter, r *http.Request) 
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(mapper.ToAPIUser(updatedUser))
 }
 
