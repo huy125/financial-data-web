a67288f47b0a9eb8a89a8cc5ca65a4db40d19b2b
Huy <dinhhuypfiev@gmail.com>
2024-11-24 22:49:04 +0100

    feat: revamp unit tests for users

--- api/users_test.go
+++ api/users_test.go
@@ -3,14 +3,17 @@ package api_test
 import (
 	"bytes"
 	"context"
-	"encoding/json"
+	"errors"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/huy125/financial-data-web/api"
+	"github.com/huy125/financial-data-web/api/dto"
+	"github.com/huy125/financial-data-web/api/store"
 	model "github.com/huy125/financial-data-web/api/store/models"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -21,9 +24,12 @@ type storeMock struct {
 	mock.Mock
 }
 
-func (m *storeMock) Create(_ context.Context, user model.User) error {
+func (m *storeMock) Create(_ context.Context, user *model.User) (*model.User, error) {
 	args := m.Called(user)
-	return args.Error(0)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*model.User), args.Error(1)
 }
 
 func (m *storeMock) List(_ context.Context, limit, offset int) ([]model.User, error) {
@@ -40,28 +46,65 @@ func (m *storeMock) Find(_ context.Context, id uuid.UUID) (*model.User, error) {
 	return nil, args.Error(1)
 }
 
-func (m *storeMock) Update(_ context.Context, id uuid.UUID, userUpdate model.UserUpdate) error {
-	args := m.Called(id, userUpdate)
-	return args.Error(0)
+func (m *storeMock) Update(_ context.Context, user *model.User) (*model.User, error) {
+	args := m.Called(user)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*model.User), args.Error(1)
 }
 
 func TestServer_CreateUserHandler(t *testing.T) {
 	tests := []struct {
 		name string
 
-		sendBody           string
-		wantUser           model.User
-		expectedStatusCode int
-		expectedResponse   any
+		sendBody      string
+		wantUserDto   *dto.UserDto
+		wantUserModel *model.User
+
+		returnErr error
+
+		wantStatus int
+		wantResult []byte
 	}{
 		{
 			name: "creates user successfully",
 
-			sendBody: `{"email":"test@example.com", "hash":"hashedpassword"}`,
+			sendBody: `{"email": "test@example.com", "firstname": "Alice", "lastname": "Smith"}`,
 
-			wantUser:           model.User{Username: "test@example.com", Hash: "hashedpassword"},
-			expectedStatusCode: http.StatusCreated,
-			expectedResponse:   "User created successfully",
+			wantUserDto: &dto.UserDto{Email: "test@example.com", Firstname: "Alice", Lastname: "Smith"},
+			wantUserModel: &model.User{
+				Id:        uuid.MustParse("ab678e01-00ee-4e4c-acfc-6dc0b68fee20"),
+				Email:     "test@example.com",
+				Firstname: "Alice",
+				Lastname:  "Smith",
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+			},
+			returnErr: nil,
+
+			wantStatus: http.StatusCreated,
+			wantResult: []byte(`{"id": "ab678e01-00ee-4e4c-acfc-6dc0b68fee20", "email": "test@example.com", "firstname": "Alice", "lastname": "Smith"}`),
+		},
+		{
+			name: "handles internal server error",
+
+			sendBody: `{"email": "test@example.com", "firstname": "Alice", "lastname": "Smith"}`,
+
+			wantUserDto:   &dto.UserDto{Email: "test@example.com", Firstname: "Alice", Lastname: "Smith"},
+			wantUserModel: nil,
+			returnErr:     errors.New("internal error"),
+
+			wantStatus: http.StatusInternalServerError,
+		},
+		{
+			name: "handles bad request error",
+
+			sendBody:      "invalid request",
+			wantUserDto:   nil,
+			wantUserModel: nil,
+
+			wantStatus: http.StatusBadRequest,
 		},
 	}
 
@@ -70,8 +113,14 @@ func TestServer_CreateUserHandler(t *testing.T) {
 			t.Parallel()
 
 			store := &storeMock{}
-			if test.wantUser != (model.User{}) {
-				store.On("Create", test.wantUser).Return(nil)
+			if test.wantUserDto != nil {
+				store.On("Create",
+					mock.MatchedBy(func(u *model.User) bool {
+						return u.Email == test.wantUserDto.Email &&
+							u.Firstname == test.wantUserDto.Firstname &&
+							u.Lastname == test.wantUserDto.Lastname
+					}),
+				).Return(test.wantUserModel, test.returnErr)
 			}
 
 			apiKey := "testApiKey"
@@ -84,17 +133,18 @@ func TestServer_CreateUserHandler(t *testing.T) {
 
 			var req *http.Request
 			req, _ = http.NewRequest(http.MethodPost, reqURL, bytes.NewBufferString(test.sendBody))
-
 			rr := httptest.NewRecorder()
 
 			srv.ServeHTTP(rr, req)
 
-			assert.Equal(t, test.expectedStatusCode, rr.Code)
+			assert.Equal(t, test.wantStatus, rr.Code)
 
-			var res string
-			err := json.Unmarshal(rr.Body.Bytes(), &res)
-			require.NoError(t, err)
-			assert.Equal(t, test.expectedResponse, res)
+			if test.wantResult != nil {
+				res, err := io.ReadAll(rr.Body)
+				require.NoError(t, err)
+
+				assert.JSONEq(t, string(test.wantResult), string(res))
+			}
 
 			store.AssertExpectations(t)
 		})
@@ -109,44 +159,57 @@ func TestServer_UpdateUserHandler(t *testing.T) {
 		uri      string
 		sendBody string
 
-		existUser      *model.User
-		wantUser     model.UserUpdate
-		shouldMockFind bool
+		wantUserDto   *dto.UserDto
+		wantUserModel *model.User
+		returnErr     error
 
 		expectedStatusCode int
 		expectedResponse   []byte
 	}{
 		{
 			name:     "updates user successfully",
-			uri:      "/users/" + id.String(),
-			sendBody: `{"username":"test1@example.com"}`,
+			sendBody: `{"id": "` + id.String() + `", "email": "test@example.com", "firstname": "Bob", "lastname": "Smith"}`,
 
-			existUser:      &model.User{ID: id, Username: "test@example.com", Hash: "hashpassword"},
-			wantUser:     model.UserUpdate{Username: "test1@example.com"},
-			shouldMockFind: true,
+			wantUserDto: &dto.UserDto{Id: id.String(), Email: "test@example.com", Firstname: "Bob", Lastname: "Smith"},
+			wantUserModel: &model.User{
+				Id:        id,
+				Email:     "test@example.com",
+				Firstname: "Bob",
+				Lastname:  "Smith",
+				CreatedAt: time.Date(2024, 11, 24, 21, 58, 00, 00, time.UTC),
+				UpdatedAt: time.Now(),
+			},
+			returnErr: nil,
 
 			expectedStatusCode: http.StatusOK,
-			expectedResponse:   []byte(`{"message":"User updated successfully"}`),
+			expectedResponse:   []byte(`{"id": "` + id.String() + `", "email": "test@example.com", "firstname": "Bob", "lastname": "Smith"}`),
 		},
 		{
-			name:     "handles user not found",
-			uri:      "/users/" + id.String(),
-			sendBody: `{"username":"test1@example.com"}`,
+			name:     "handles user not found error",
+			sendBody: `{"id": "` + id.String() + `", "email": "test@example.com", "firstname": "Bob", "lastname": "Smith"}`,
 
-			existUser:      nil,
-			wantUser:     model.UserUpdate{},
-			shouldMockFind: true,
+			wantUserDto:   &dto.UserDto{Id: id.String(), Email: "test@example.com", Firstname: "Bob", Lastname: "Smith"},
+			wantUserModel: nil,
+			returnErr:     store.ErrNotFound,
 
 			expectedStatusCode: http.StatusNotFound,
 		},
 		{
-			name:     "handles invalid id",
-			uri:      "/users/invalid-id",
-			sendBody: `{"username":"test1@example.com"}`,
+			name:     "handles internal server error",
+			sendBody: `{"id": "` + id.String() + `", "email": "test@example.com", "firstname": "Bob", "lastname": "Smith"}`,
 
-			existUser:      nil,
-			wantUser:     model.UserUpdate{},
-			shouldMockFind: false,
+			wantUserDto:   &dto.UserDto{Id: id.String(), Email: "test@example.com", Firstname: "Bob", Lastname: "Smith"},
+			wantUserModel: nil,
+			returnErr:     errors.New("internal error"),
+
+			expectedStatusCode: http.StatusInternalServerError,
+		},
+		{
+			name:     "handles bad request error",
+			sendBody: "invalid request",
+
+			wantUserDto:   nil,
+			wantUserModel: nil,
 
 			expectedStatusCode: http.StatusBadRequest,
 		},
@@ -157,12 +220,15 @@ func TestServer_UpdateUserHandler(t *testing.T) {
 			t.Parallel()
 
 			store := &storeMock{}
-			if test.shouldMockFind {
-				store.On("Find", id).Return(test.existUser, nil)
-			}
-
-			if test.wantUser != (model.UserUpdate{}) {
-				store.On("Update", id, test.wantUser).Return(nil)
+			if test.wantUserDto != nil {
+				store.On("Update",
+					mock.MatchedBy(func(u *model.User) bool {
+						return u.Id.String() == test.wantUserDto.Id &&
+							u.Email == test.wantUserDto.Email &&
+							u.Firstname == test.wantUserDto.Firstname &&
+							u.Lastname == test.wantUserDto.Lastname
+					}),
+				).Return(test.wantUserModel, test.returnErr)
 			}
 
 			apiKey := "testApiKey"
@@ -171,7 +237,7 @@ func TestServer_UpdateUserHandler(t *testing.T) {
 			httpSrv := httptest.NewServer(srv)
 			t.Cleanup(func() { httpSrv.Close() })
 
-			reqURL := httpSrv.URL + test.uri
+			reqURL := httpSrv.URL + "/users/" + id.String()
 
 			req, err := http.NewRequest(http.MethodPatch, reqURL, bytes.NewBufferString(test.sendBody))
 			require.NoError(t, err)
